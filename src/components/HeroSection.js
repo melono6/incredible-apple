fd6d843b0f1c351c9a4009508cb24a1788c6ab38
Jason Emery <jason.emery@rufusleonard.com>
2021-03-03 17:21:36 +0000

    rewrite heroSection

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,48 +1,54 @@
 import React from 'react';
-import _ from 'lodash';
 
 import {classNames, toStyleObj, withPrefix} from '../utils';
 import SectionActions from './SectionActions';
 
-export default class HeroSection extends React.Component {
-    render() {
-        let section = _.get(this.props, 'section', null);
-        let background = _.get(section, 'background', null);
-        let background_color = _.get(background, 'background_color', null) || 'white';
-        let background_opacity_pct = _.get(background, 'background_image_opacity', null) || 100;
-        let background_opacity = background_opacity_pct * 0.01;
-        let background_size = _.get(background, 'background_image_size', null) || 'cover';
-        let background_repeat = _.get(background, 'background_image_repeat', null) || 'no-repeat';
-        return (
-            <section className={classNames('section', 'hero', {'bg-image': _.get(section, 'has_background', null) && _.get(background, 'background_image', null), 'inverse bg-blue': _.get(section, 'has_background', null) && (background_color === 'blue'), 'bg-gray': _.get(section, 'has_background', null) && (background_color === 'gray'), 'section--padding': _.get(section, 'has_background', null) || _.get(section, 'image', null)})}>
-              {(_.get(section, 'has_background', null) && _.get(background, 'background_image', null)) && (
-              <div className="bg-image__image" style={toStyleObj('background-image: url(\'' + withPrefix(_.get(background, 'background_image', null)) + '\'); opacity: ' + background_opacity + '; background-size: ' + background_size + '; background-repeat: ' + background_repeat)}/>
+export const HeroSection = ( props ) => {
+  const { section } = props;
+  const { background, has_background, title, subtitle, actions, image, image_alt, image_position, align } = section;
+
+  const {
+    background_color,
+    background_image,
+    background_image_opacity: background_opacity_pct,
+    background_image_size: background_size,
+    background_image_repeat: background_repeat,
+  } = background;
+
+  const background_opacity = background_opacity_pct * 0.01;
+
+  return (
+      <section className={classNames('section', 'hero', {'bg-image': has_background && background_image, 'inverse bg-blue': has_background && (background_color === 'blue'), 'bg-gray': has_background && (background_color === 'gray'), 'section--padding': has_background || image})}>
+        {has_background && background_image) && (
+        <div className="bg-image__image" style={toStyleObj('background-image: url(\'' + withPrefix(background_image) + '\'); opacity: ' + background_opacity + '; background-size: ' + background_size + '; background-repeat: ' + background_repeat)}/>
+        )}
+        <div className="container container--lg">
+          <div className={classNames('flex', 'flex--middle', 'flex--center', 'flex--col-2', {'align-center': align === 'center', 'align-right': align === 'right'})}>
+            {image && (
+            <div className={classNames('cell', 'section__media', {'section__media--right': image_position === 'right'})}>
+              <img src={withPrefix(image)} alt={image_alt} />
+            </div>
+            )}
+            <div className="cell section__body">
+              {title && (
+              <h1 className="section__title">{title}</h1>
               )}
-              <div className="container container--lg">
-                <div className={classNames('flex', 'flex--middle', 'flex--center', 'flex--col-2', {'align-center': _.get(section, 'align', null) === 'center', 'align-right': _.get(section, 'align', null) === 'right'})}>
-                  {_.get(section, 'image', null) && (
-                  <div className={classNames('cell', 'section__media', {'section__media--right': _.get(section, 'image_position', null) === 'right'})}>
-                    <img src={withPrefix(_.get(section, 'image', null))} alt={_.get(section, 'image_alt', null)} />
-                  </div>
-                  )}
-                  <div className="cell section__body">
-                    {_.get(section, 'title', null) && (
-                    <h1 className="section__title">{_.get(section, 'title', null)}</h1>
-                    )}
-                    {_.get(section, 'subtitle', null) && (
-                    <div className="section__copy">
-                      <p>{_.get(section, 'subtitle', null)}</p>
-                    </div>
-                    )}
-                    {_.get(section, 'actions', null) && (
-                    <div className="section__actions btn-group">
-                      <SectionActions {...this.props} actions={_.get(section, 'actions', null)} />
-                    </div>
-                    )}
-                  </div>
-                </div>
+              {subtitle&& (
+              <div className="section__copy">
+                <p>{subtitle}</p>
               </div>
-            </section>
-        );
-    }
+              )}
+              {actions && (
+              <div className="section__actions btn-group">
+                <SectionActions {...props} actions={actions} />
+              </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </section>
+  );
+
 }
+
+export default HeroSection;
